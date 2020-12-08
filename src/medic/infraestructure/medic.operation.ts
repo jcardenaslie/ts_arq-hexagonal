a9a28eda4824498c8d293c3928c62ac29757ae70
Joaquin Cardenas <joaquin.cardenas@almacenguru.com>
2020-12-08 00:07:05 -0300

    Clean and add MedicType

--- src/medic/infraestructure/medic.operation.ts
+++ src/medic/infraestructure/medic.operation.ts
@@ -1,9 +1,8 @@
-import { GenericOperation } from "../../generic/generic.operation";
+import { MedicType } from './medic.type';
 import GenericDatabaseRepository from "../../repositories/generic.repositories";
-import { Medic } from "../domain/entities/medic.entity";
 import MedicModel from "./medic.model"
 
-export class MedicOperation extends GenericDatabaseRepository< typeof MedicModel, Medic> {
+export class MedicOperation extends GenericDatabaseRepository< typeof MedicModel, MedicType> {
   constructor(){
     super(MedicModel)
   }
@@ -11,23 +10,4 @@ export class MedicOperation extends GenericDatabaseRepository< typeof MedicModel
   getByLocations() {
     console.log("List medics by locations");
   }
-  // async insert(medic: Medic): Promise<Medic> {
-  //   const medicInserted = await MedicModel.create(medic)
-  //   return medicInserted;
-  // }
-  // async update(id: string | number, medic: Medic): Promise<Medic> {
-  //   return medic;
-  // }
-  // async get(isActive: boolean): Promise<Medic[]> {
-  //   const listMedic: Medic[] = [];
-  //   return listMedic;
-  // }
-  // async getOne(id: string | number): Promise<Medic> {
-  //   const medic: Medic = {};
-  //   return medic;
-  // }
-  // async delete(id: string | number): Promise<Medic> {
-  //   const medic: Medic = {};
-  //   return medic;
-  // }
 }
